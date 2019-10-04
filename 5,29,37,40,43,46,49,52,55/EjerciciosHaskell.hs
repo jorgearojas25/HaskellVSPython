e4a501cbcabb91a7446dbbef6307b96b3c676050
Alvaro <aazarabandag@correo.udistrital.edu.co>
2019-10-04 17:58:03 -0500

    ejercicios correspondientes

--- 5,29,37,40,43,46,49,52,55/EjerciciosHaskell.hs
+++ 5,29,37,40,43,46,49,52,55/EjerciciosHaskell.hs
@@ -1,1 +1,36 @@
 -- ejercicios
+
+--funcion para hallar el cuadrado de una lista de enteros
+cuadrados::[Int]->[Int]
+cuadrados [ ] = [ ]
+cuadrados l = [x*x| x <- l]
+
+--prueba de cualificadores
+cualif1 = print([x | x <- [1 .. 12]])
+cualif2 = print([x+x | x <- [1 .. 12]])
+cualif3 = print([x*x | x <- [1..10]])
+
+--funcion para hallar el factorial de un numero
+factorial :: Integer -> Integer
+factorial n = if n == 0 then 1 else n * factorial (n-1)
+
+--prueba de funciones incluidas en haskell
+tamañolista = print(length [2,3,9])
+concatlistas = print([1,3,10] ++ [2,6,5,7])
+mapf =  print(map fromEnum ['H', 'o', 'l', 'a'])
+posicion = print(["maritza","celeste","nadia","maria","julia"]!!1)
+cabeza = print(head [11,1,1985,22,8,2007])
+cola = print(last [11,1,1985,22,8,2007])
+restriccion = print(dropWhile (<=15) [1..30])
+invertirlista = print (reverse [11,1,1985,22,8,2007]) 
+concatlista = print(concat ["open","source","solutions"])
+
+--funcion para comprobar si una lista esta ordenada 
+lista_ordenada::Ord a=>[a]->Bool
+lista_ordenada [] = True
+lista_ordenada [_] = True
+lista_ordenada (x:y:xs) = (x<=y) && lista_ordenada (y:xs)
+
+--prueba de listas por comprehension
+lista1 = print([ x | x<-[[1,2],[3,4]], x<-x ])
+lista2 = print([ x | x<-[1,2], x<-[3,4] ])
--- 5,29,37,40,43,46,49,52,55/EjerciciosPython.py
+++ 5,29,37,40,43,46,49,52,55/EjerciciosPython.py
@@ -1,1 +1,54 @@
 # ejercicios
+
+#funcion para hallar el cuadrado de una lista de numeros
+def cuadrados(list):
+    if(list==[]):
+        return []
+    list[0]=list[0]*list[0]
+    return [list[0]]+cuadrados(list[1::])
+
+lista1 = [9,8,5,3,1];
+print("cuadrados de la lista: ",cuadrados(lista1))
+
+#funcion para hallar el factorial de un numero
+def factorial(n):
+    if(n == 0):
+        return 1;
+    else:
+        return n * factorial (n-1);
+
+
+#funcion para invertir una lista dada
+def rev(l):
+    if len(l) == 0:
+        return []
+    return [l[-1]] + rev(l[:-1]);
+print("lista invertida ",rev([1,2,3,4,5]))
+
+#funcion para comprobar el orden de una lista
+def listaOrden(list):
+    if(list==[]):
+        return True
+    if(list[1::]==[]):
+        return True
+    if((list[:1]<=list[1:2])&listaOrden(list[1::])):
+            return True
+    return False
+
+a=[1,2,3,4,5,6,7,8,9,10]
+b=[5,4,1,2,3,33,15,8]
+print("comprobacion orden lista a: ",listaOrden(a))
+print("comprobacion orden lista b: ",listaOrden(b))
+
+#prueba de la funcion len y concatenamiento de listas
+lista1 = [1,2,3,6,4];
+print ("longitud de la lista: ",len(lista1));
+
+lista2 = [1,2,3,4,5,6,7,2,123,88];
+
+lista1lista2 = lista1 + lista2;
+print("listas concatenadas: ",lista1lista2);
+
+if __name__ == "__main__":
+    n = int(input("Ingrese un numero para hallar su factorial: "))
+    print("El factorial de ",n," es: ",factorial(n)); 
