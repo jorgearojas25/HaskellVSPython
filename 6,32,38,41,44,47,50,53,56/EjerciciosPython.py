a736c50e6bccb7d32afdd65f983abc48347abf1b
Daniela Alexandra <daniela.martinez@infotrack.com.co>
2019-10-04 19:54:16 -0500

    subir

--- 6,32,38,41,44,47,50,53,56/EjerciciosPython.py
+++ 6,32,38,41,44,47,50,53,56/EjerciciosPython.py
@@ -1,1 +1,34 @@
 # ejercicios
+""" Funcion factorial """
+def factorial(n):
+    return 1 if n==0 else n*factorial(n-1)
+
+""" Secuencias"""
+def Secuencia(m,n):
+  if (n == m):
+      return m
+  print(m)
+  return  Secuencia(m+1,n)
+
+""" Funcion igual lista"""
+def compare (lst1, lst2):
+    if lst1 and lst2:
+        if len(lst1) != len(lst2):
+            return False
+        if lst1[0] != lst2[0]:
+            return False
+        return compare(lst1[1:],lst2[1:])
+    else:
+        return True
+
+"""Funcion Mayor elemento Lista"""
+def mayor (m, i =1):
+    if m[i] > m[i-1]: 
+      i= i+1
+      print(m[i])
+      return mayor(m,i)
+
+
+
+print (factorial(5))
+print(Secuencia(-3,3))
